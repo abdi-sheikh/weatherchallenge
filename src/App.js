baccf556991c7e0f29a28a1ca0656a70e4bc55ad
abdi-sheikh <abdisheikh55@gmail.com>
2019-10-30 20:08:32 -0500

    Fixed api issue (actually) - renaming variables

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ class App extends React.Component{
     const country = e.target.elements.country.value;
     const state = e.target.elements.state.value;
 
-    const longlat_api = await fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${LONGLAT_API_KEY}&location=${city},${state},${country}`);
+    const longlat_api = await fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${LONGLAT_API}&location=${city},${state},${country}`);
     longlatdata = await longlat_api.json();
     const lat = longlatdata.results[0].locations[0].latLng.lat;
     const lng = longlatdata.results[0].locations[0].latLng.lng;
-    const weather_api = await fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${WEATHER_API_KEY}/${lat},${lng}?exclude=currently,minutely,hourly`);
+    const weather_api = await fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${WEATHER_API}/${lat},${lng}?exclude=currently,minutely,hourly`);
     data = await weather_api.json();
     
     this.setState({middayWeather: []});
